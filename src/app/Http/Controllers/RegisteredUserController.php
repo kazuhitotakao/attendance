4fad75ef0a597e9498c3dd055c73f75aabcb091e
takao kazuhito <simasyamu@gmail.com>
2024-05-29 10:13:31 +0900

    register ok

---
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Responses\RegisterResponse as ResponsesRegisterResponse;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Fortify\Contracts\RegisterResponse;
+use Laravel\Fortify\Fortify;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * The guard implementation.
+     *
+     * @var \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected $guard;
+
+    public function store(
+        Request $request,
+        CreatesNewUsers $creator
+    ): RegisterResponse {
+        if (config('fortify.lowercase_usernames')) {
+            $request->merge([
+                Fortify::username() => Str::lower($request->{Fortify::username()}),
+            ]);
+        }
+
+        event(new Registered($user = $creator->create($request->all())));
+
+        // $this->guard->login($user);
+
+        return app(ResponsesRegisterResponse::class);
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/', [AuthController::class, 'index']);
 });
