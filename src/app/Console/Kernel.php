78e68eb9dd587a028921cc8efc0d325026a076f0
takao kazuhito <simasyamu@gmail.com>
2024-06-07 20:08:08 +0900

    cron 80% ok

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:carryover')->daily();
+        $schedule->command('command:carryover')->dailyAt('0:00');
     }
 
     /**
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,12 +12,9 @@
     <form class="form" action="/login" method="post">
         @csrf
         <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-            </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="email" name="email" value="{{ old('email') }}" />
+                    <input type="email" name="email" placeholder="メールアドレス" value="{{ old('email') }}" />
                 </div>
                 <div class="form__error">
                     @error('email')
@@ -27,12 +24,9 @@
             </div>
         </div>
         <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">パスワード</span>
-            </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="password" name="password" />
+                    <input type="password" name="password" placeholder="パスワード" />
                 </div>
                 <div class="form__error">
                     @error('password')
@@ -45,8 +39,13 @@
             <button class="form__button-submit" type="submit">ログイン</button>
         </div>
     </form>
+    <div class="register__link-message">
+        <span class="register__link-message__text">
+            アカウントをお持ちでない方はこちらから
+        </span>
+    </div>
     <div class="register__link">
-        <a class="register__button-submit" href="/register">会員登録の方はこちら</a>
+        <a class="register__button-submit" href="/register">会員登録</a>
     </div>
 </div>
 @endsection
