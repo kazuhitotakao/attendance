f4fcbf5d60cfdef72555c09cc3b80e70c1ae25bc
takao kazuhito <simasyamu@gmail.com>
2024-06-16 23:07:50 +0900

    メール認証　メールの内容を日本語に変更

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,2 +1,33 @@
-確認メールを送っているよ
-メールで本登録をしてね。
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/verify-email.css') }}">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <a class="header__logo" href="/">
+                    Atte
+                </a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="mail__content">
+            <p>メール確認してね</p>
+        </div>
+    </main>
+    <footer class="footer">
+        <small class="footer__logo">Atte,inc.</small>
+    </footer>
+</body>
+
+</html>
