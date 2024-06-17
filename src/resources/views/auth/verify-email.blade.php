ae1f58f0fde79d076b53fc35daac023e9f4e5cf9
takao kazuhito <simasyamu@gmail.com>
2024-06-17 20:48:01 +0900

    メール認証機能テスト環境で完成

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -22,7 +22,20 @@
     </header>
     <main>
         <div class="mail__content">
-            <p>メール確認してね</p>
+            <div class="card">
+                <h3 class="card__title">
+                    サービスのご利用ありがとうございます！<br>
+                    登録作業はまだ完了しておりません。<br>
+                    お手数ですが、以下のメール認証作業をお願いします。
+                </h3>
+                <div class="card__verify">
+                    <ol>
+                        <li>登録したメールアドレスのメールボックスを開く</li>
+                        <li>「メールアドレスの認証」のメールを開く</li>
+                        <li>メール本文中の「メールアドレスの認証」ボタンをクリック</li>
+                    </ol>
+                </div>
+            </div>
         </div>
     </main>
     <footer class="footer">
